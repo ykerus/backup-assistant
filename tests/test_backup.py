df78251ce401c3ccfec8673e6f9050dab33bb9e8
ykerus <yke.rusticus@gmail.com>
2024-12-20 10:50:37 +0100

    Add test for deleted files

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from typing import Dict
-from backup_assistant.backup import get_file_paths_with_modified_dates, get_files_to_backup
+from backup_assistant.backup import (
+    get_file_paths_with_modified_dates,
+    get_files_to_backup,
+    get_files_to_delete,
+)
 from backup_assistant.config import Config, load_config
 
 import pytest
@@ -27,12 +31,12 @@ def test_get_files_to_backup(
     backup_files_list = get_files_to_backup(from_folder_files, to_folder_files, config)
 
     expected_files = [
-        Path("file_1.txt"),
+        # Path("file_1.txt"),  # unmodified
         Path("file_2.txt"),
         Path("folder_1/file_1_1.txt"),
         # Path("folder_1/file_1_2.mp4"),  # ignore
-        Path("folder_1/subfolder_1_1/file_1_1_1.txt"),
-        Path("folder_1/subfolder_1_1/file_1_1_2.txt"),
+        Path("folder_1/subfolder_1_1/file_1_1_1.txt"),  # modified
+        # Path("folder_1/subfolder_1_1/file_1_1_2.txt"),  # unmodified
         Path("folder_2/file_2_1.txt"),
         Path("folder_2/file_2_2.png"),
         # Path("folder_2/subfolder_1_1/*"),  # ignore
@@ -41,8 +45,29 @@ def test_get_files_to_backup(
         # Path("folder_3/*"),  # ignore
     ]
 
-    in_backup_not_in_expected = set(expected_files) - set(backup_files_list)
-    in_expected_not_in_backup = set(backup_files_list) - set(expected_files)
+    in_expected_not_in_backup = set(expected_files) - set(backup_files_list)
+    in_backup_not_in_expected = set(backup_files_list) - set(expected_files)
 
     msg = f"\n\n{in_backup_not_in_expected=}\n\n{in_expected_not_in_backup=}\n"
     assert len(in_backup_not_in_expected) == 0 and len(in_expected_not_in_backup) == 0, msg
+
+
+def test_get_files_to_delete(
+    from_folder_files: Dict[Path, Dict], to_folder_files: Dict[Path, Dict]
+):
+    delete_files_list = get_files_to_delete(from_folder_files, to_folder_files)
+    delete_files_list = [file for file in delete_files_list if file.name != ".DS_Store"]
+
+    expected_files = [
+        Path("file_3.txt"),
+        Path("folder_1/file_1_3.txt"),
+        Path("folder_4/file_4_1.txt"),
+        Path("folder_4/file_4_2.mp4"),
+        Path("folder_4/subfolder_4_1/file_4_1_1.txt"),
+    ]
+
+    in_expected_not_in_deleted = set(expected_files) - set(delete_files_list)
+    in_deleted_not_in_expected = set(delete_files_list) - set(expected_files)
+
+    msg = f"\n\n{in_deleted_not_in_expected=}\n\n{in_expected_not_in_deleted=}\n"
+    assert len(in_deleted_not_in_expected) == 0 and len(in_expected_not_in_deleted) == 0, msg
